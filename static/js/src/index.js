56d37b10b9032b30b08cb524c2008ed4060b5272
Magesh Ravi <magesh@webinative.com>
2020-04-02 13:29:55 +0530

    feat: [#2] Loads data dynamically
    Updates example file `index.html` to implement the following features.
    
    - Ability to load JSON data from a textarea field and render the output.
    - Updates the output textarea to be readonly.

--- static/js/src/index.js
+++ static/js/src/index.js
@@ -1,52 +1,38 @@
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
+import List from '@editorjs/list';
 import ImageGallery from './ejsImageGallery';
 
 const editor = new EditorJS({
     holder: 'codex-editor',
     tools: {
         heading: Header,
-        gallery: ImageGallery
+        list: {
+            class: List,
+            inlineToolbar: true,
+        },
+        gallery: ImageGallery,
     },
-    data: {
-        "time": 1584459478133,
-        "blocks": [
-            {
-                "type": "heading",
-                "data": {
-                    "text": "Hello, World",
-                    "level": 1
-                }
-            },
-            {
-                "type": "paragraph",
-                "data": {
-                    "text": "Placeimg.com<br>"
-                }
-            },
-            {
-                "type": "gallery",
-                "data": {
-                    "images": [
-                        {
-                            "url": "https://placeimg.com/148/148/animals"
-                        },
-                        {
-                            "url": "https://placeimg.com/148/148/arch"
-                        },
-                        {
-                            "url": "https://placeimg.com/148/148/nature"
-                        },
-                        {
-                            "url": "https://placeimg.com/148/148/tech"
-                        },
-                    ]
-                }
-            }
-        ],
-        "version": "2.17.0"
-    }
 });
+
+const ipTextarea = document.querySelector('#codex-editor-input');
+const loadBtn = document.querySelector('#load');
+
+loadBtn.addEventListener('click', () => {
+    const data = JSON.parse(ipTextarea.value)
+
+    if (!data) {
+        return
+    }
+
+    editor.render(data)
+        .then(() => {
+            console.log('Data loaded successfully');
+        })
+        .catch((err) => {
+            console.err('Error loading data:', err);
+        })
+})
 
 const textarea = document.querySelector('#codex-editor-textarea');
 const myBtn = document.querySelector('#my-btn');
